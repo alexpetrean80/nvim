0415b7ea57e8200484120dbfadb675e56fe78d25
Alex-Tudor Petrean <alex@padfoot.cc>
2024-10-14 21:09:23 +0300

    extend mini.files

---
+++ lua/extend/mini.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "echasnovski/mini.files",
+    opts = {
+      options = {
+        use_as_default_explorer = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function() 
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Explorer",
+      }
+    },
+  }
+}
