d98d8b23abae62066a292b3d50169b94b267731d
Alex-Tudor Petrean <alex-tudor.petrean@snyk.io>
2024-10-15 11:10:01 +0300

    disable backup and add word wrap

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.backup = false
+vim.opt.writebackup = false
 vim.opt.swapfile = false
+vim.opt.wrap = true
+
 vim.g.lazy_picker = "telescope"
 vim.g.lazyvim_python_lsp = "ruff"
